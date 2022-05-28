d63411edd9fd3093f77bc223d7d6d7357653bfee
Dylan <dylan.reeder5@gmail.com>
2022-05-28 19:33:27 -0400

    added similar logic to upload album cover to bucket

--- controllers/albums.go
+++ controllers/albums.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"context"
+
+	"github.com/carbondesigned/backstage-features-service/config"
 	database "github.com/carbondesigned/backstage-features-service/database"
 	models "github.com/carbondesigned/backstage-features-service/models"
 	"github.com/carbondesigned/backstage-features-service/utils"
@@ -19,7 +22,7 @@ func CreateAlbum(c *fiber.Ctx) error {
 		})
 	}
 
-	// is the user an author
+	// is the usera an author
 	token := c.Get("Authorization")
 	claims, err := jwt.Parse(
 		token,
@@ -37,8 +40,9 @@ func CreateAlbum(c *fiber.Ctx) error {
 	}
 
 	id := claims.Claims.(jwt.MapClaims)["id"]
+	author := models.Author{}
 
-	err = database.DB.Db.Where("id = ?", id).First(&models.Author{}).Error
+	err = database.DB.Db.Where("id = ?", id).First(&author).Error
 
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -47,6 +51,18 @@ func CreateAlbum(c *fiber.Ctx) error {
 			"error":   err.Error(),
 		})
 	}
+	// we process the image and upload it to a bucket
+	cover := album.Cover
+	coverURL, err := config.UploadImage(context.TODO(), int(author.ID), cover)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"success": false,
+			"message": "Error trying to upload image",
+			"error":   err.Error(),
+		})
+	}
+	// we set the coverURL to the post
+	album.CoverURL = coverURL
 
 	album.Slug = utils.GenerateSlugFromTitle(album.Title)
 	if err := database.DB.Db.Create(&album).Error; err != nil {
